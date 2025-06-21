24ac85ee71ac70f30b8dd56446252584b3f74c9e
Strahinja420 <strahidelijica1@gmail.com>
2025-06-21 17:40:34 +0200

    fixed topbar look

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import AddAuction from "./pop-ups/AddAuction";
 import ProfileSettings from "./pop-ups/ProfileSettings";
+import { Bell, User } from "lucide-react";
+import { House } from 'lucide-react';
 
 const TopBar = () => {
   const [addAuctionModal, setAddAuctionModal] = useState(false);
@@ -27,12 +29,8 @@ const TopBar = () => {
               }`
             }
           >
-            <img
-              className="bg-white rounded-2xl"
-              src="src/assets/icons/home.png"
-              alt=""
-            />
-            Auctions
+            <House size={16} />
+            <p className="pl-1">Auctions</p>
           </NavLink>
         </div>
         <div className="flex">
@@ -46,60 +44,43 @@ const TopBar = () => {
               }`
             }
           >
-            <img
-              className="bg-white rounded-2xl"
-              src="src/assets/icons/Person.png"
-              alt=""
-            />
-            Profile
+            <User size={16} />
+            <p className="pl-1">Profile</p>
           </NavLink>
         </div>
       </div>
 
       <div className="flex-grow"></div>
-
-      {/* //TODO promeni sliku u svg ikonicu iz lucide react-a */}
-      <div className="flex items-center space-x-4">
-        <div className="flex items-center bg-white p-0 rounded-[32px]">
-          <button className="relative p-2 pr-0 text-gray-600 hover:text-gray-900">
-            <img
-              className="w-8 h-8 bg-gray-400 rounded-full"
-              src="src/assets/icons/Notifications.png"
-              alt=""
-            />
-            {/* <span className="absolute top-0 right-0 w-2 h-2 bg-red-500 rounded-full"></span> */}
-          </button>
-          <div className="px-2">
-            <button
-              onClick={() => setAddAuctionModal(!addAuctionModal)}
-              className="hover:cursor-pointer"
-            >
-              <img
-                className="h-11 "
-                src="src/assets/images/button.png"
-                alt=""
-              />
-            </button>
-            {addAuctionModal && (
-              <AddAuction
-                onClose={() => setAddAuctionModal(!addAuctionModal)}
-              />
-            )}
+      <div className="flex items-center bg-white  rounded-[32px] gap-2 p-1 max-h-[64px] ">
+        <button className="relative text-gray-600 hover:text-gray-900">
+          <div className="text-black bg-(--lighter-gray) rounded-[32px] p-3.5 ">
+            <Bell size={15} />
           </div>
-          <button
-            onClick={() => setProfileSettings(!profileSettings)}
-            className="hover:cursor-pointer"
-          >
-            <div className="flex items-center justify-center bg-gray-300 rounded-full h-11 w-11">
-              <span className="text-sm font-medium">JR</span>
-            </div>
-          </button>
-          {profileSettings && (
-                <ProfileSettings
-                  onClose={() => setProfileSettings(!profileSettings)}
-                />
-              )}
-        </div>
+          {/* <span className="absolute top-0 right-0 w-2 h-2 bg-red-500 rounded-full"></span> */}
+        </button>
+        <button
+          onClick={() => setAddAuctionModal(!addAuctionModal)}
+          className="hover:cursor-pointer "
+        >
+          <img className="h-11 " src="src/assets/images/button.png" alt="" />
+        </button>
+        {addAuctionModal && (
+          <AddAuction onClose={() => setAddAuctionModal(!addAuctionModal)} />
+        )}
+
+        <button
+          onClick={() => setProfileSettings(!profileSettings)}
+          className="hover:cursor-pointer"
+        >
+          <div className="flex items-center justify-center bg-gray-300 rounded-full h-11 w-11">
+            <span className="text-sm font-medium">JR</span>
+          </div>
+        </button>
+        {profileSettings && (
+          <ProfileSettings
+            onClose={() => setProfileSettings(!profileSettings)}
+          />
+        )}
       </div>
     </div>
   );
